c6b9859c4656513aa8eb6b229b75d12636d65235
Ivan Angelov <ivangotoy@gmail.com>
2024-06-12 15:37:28 +0300

    Optimize feishin deployment job

--- pipeline_feishin_build_deploy.groovy
+++ pipeline_feishin_build_deploy.groovy
@@ -27,19 +27,21 @@ pipeline {
             steps {
                 dir('feishin') {
                     script {
-                        def Command1 = "sed -i 's/ --mac//g' package.json"
-                        sh Command1
-                        sh 'npm install --legacy-peer-deps'
-                        sh 'npm run package:pr'
-	  	                sh "ssh root@alder 'mkdir -p /var/www/html/files/feishin/feishin-$DATE'"
-		                sh "rsync release/build/Feishin-*-linux-x64.tar.xz release/build/Feishin-*-win-x64.exe root@alder:/var/www/html/files/feishin/feishin-$DATE/"
-		                sh 'ssh root@alder chown -R http: /var/www/html/files/feishin/'
-		                sh "ssh root@alder 'bash /tmp/cleanup_builds.sh'"
-                    }
-                }
+                        def DATE = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy-HH-mm-ss"))
+                        sh '''
+                        sed -i 's/ --mac//g' package.json
+                        npm install --legacy-peer-deps
+                        npm run package:pr
+	  	                ssh root@alder 'mkdir -p /var/www/html/files/feishin/feishin-$DATE'
+		                rsync release/build/Feishin-*-linux-x64.tar.xz release/build/Feishin-*-win-x64.exe root@alder:/var/www/html/files/feishin/feishin-$DATE/
+		                ssh root@alder chown -R http: /var/www/html/files/feishin/
+		                ssh root@alder 'bash /tmp/cleanup_builds.sh'
+		                '''
             }
         }
-        stage('PHASE4: LAST Clean WORKSPACE') {
+    }
+}
+stage('PHASE4: LAST Clean WORKSPACE') {
             steps {
                 cleanWs(deleteDirs: true, disableDeferredWipeout: true)
             }
