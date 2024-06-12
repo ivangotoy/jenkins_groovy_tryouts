1e03aadbc19a844c89970835d3e52bc9f3bf322d
Ivan Angelov <ivangotoy@gmail.com>
2024-06-12 15:00:58 +0300

    OPTIMIZE TOOLS DEPLOYMENT PIPELINE BY AAAAALOTTTTT

--- pipeline_tools_build_deploy.groovy
+++ pipeline_tools_build_deploy.groovy
@@ -1,97 +1,186 @@
 pipeline {
-	agent {
-		label 'ryzen9'
-	}
+    agent {
+        label 'ryzen9'
+    }
 
-	options {
-		timestamps()
-		skipDefaultCheckout(true)
-	}
+    options {
+        timestamps()
+        skipDefaultCheckout(true)
+    }
 
-	environment {
-		PATH = "/usr/local/go/bin:${env.PATH}"
-		GOOS = "linux"
-		GOARCH = "amd64"
-		GOAMD64 = "v3"
-		GOEXPERIMENT = "newinliner"
-	}
+    environment {
+        PATH = "/usr/local/go/bin:${env.PATH}"
+        GOOS = "linux"
+        GOARCH = "amd64"
+        GOAMD64 = "v3"
+        LC_ALL = "C"
+        LANG = "C"
+        GOEXPERIMENT = "newinliner"
+    }
 
-	stages {
-		stage('PHASE1: CLEAN WORKSPACE') {
-			steps {
-				cleanWs(deleteDirs: true, disableDeferredWipeout: true)
-			}
-		}
+    stages {
+        stage('PHASE1: CLEAN WORKSPACE') {
+            steps {
+                cleanWs(deleteDirs: true, disableDeferredWipeout: true)
+            }
+        }
 
-		stage('PHASE2: GIT CLONE TOOLS') {
-			steps {
-				sh 'printf "https://github.com/goreleaser/goreleaser\nhttps://github.com/upx/upx.git\nhttps://github.com/google/go-containerregistry\nhttps://github.com/aquasecurity/trivy.git" | xargs -I{} -P4 git clone --quiet --depth 1 --single-branch {}'
-			}
-		}
+        stage('PHASE2: GIT CLONE TOOLS') {
+            steps {
+                script {
+                    try {
+                        parallel (
+                            "Clone goreleaser": {
+                                sh "git clone --quiet --depth 1 --single-branch https://github.com/goreleaser/goreleaser"
+                            },
+                            "Clone upx": {
+                                sh "git clone --quiet --depth 1 --single-branch https://github.com/upx/upx.git"
+                            },
+                            "Clone go-containerregistry": {
+                                sh "git clone --quiet --depth 1 --single-branch https://github.com/google/go-containerregistry"
+                            },
+                            "Clone trivy": {
+                                sh "git clone --quiet --depth 1 --single-branch https://github.com/aquasecurity/trivy.git"
+                            }
+                        )
+                    } catch (Exception e) {
+                        echo "Error during cloning repositories: ${e.message}"
+                        currentBuild.result = 'FAILURE'
+                        error("Stopping pipeline due to failed git clone.")
+                    }
+                }
+            }
+        }
 
-		stage('PHASE3: BUILD & DEPLOY TOOLS') {
-			steps {
-				dir('upx') {
-					sh 'git submodule update --init'
-					sh 'make build/release -j17'
-					sh 'strip -s build/release/upx'
-					sh 'upx -9 -q build/release/upx'
-					sh 'sudo cp build/release/upx /usr/local/bin'
-				}
+        stage('PHASE3: BUILD UPX') {
+            steps {
+                script {
+                    try {
+                        dir('upx') {
+                            sh '''
+                            echo "Environment Variables:"
+                            echo "LC_ALL=$LC_ALL"
+                            echo "LANG=$LANG"
+                            echo "PATH=$PATH"
+                            git submodule update --init
+                            make build/release -j$(nproc)
+                            strip -s build/release/upx
+                            upx -9 -q build/release/upx
+                            sudo cp build/release/upx /usr/local/bin
+                            '''
+                        }
+                    } catch (Exception e) {
+                        echo "Error during building UPX: ${e.message}"
+                        currentBuild.result = 'FAILURE'
+                        error("Stopping pipeline due to failed UPX build.")
+                    }
+                }
+            }
+        }
 
-				dir('goreleaser') {
-					sh 'go mod tidy'
-					sh 'go build -ldflags="-s -w" -trimpath -pgo=auto -o goreleaser .'
-					sh 'upx -9 -q goreleaser'
-					sh 'sudo cp goreleaser /usr/local/bin'
-				}
+        stage('PHASE4: BUILD GORELEASER') {
+            steps {
+                script {
+                    try {
+                        dir('goreleaser') {
+                            sh '''
+                            echo "Environment Variables:"
+                            echo "LC_ALL=$LC_ALL"
+                            echo "LANG=$LANG"
+                            echo "PATH=$PATH"
+                            echo "GOOS=$GOOS"
+                            echo "GOARCH=$GOARCH"
+                            echo "GOAMD64=$GOAMD64"
+                            echo "GOEXPERIMENT=$GOEXPERIMENT"
+                            go mod tidy
+                            go build -ldflags="-s -w" -trimpath -pgo=auto -o goreleaser .
+                            upx -9 -q goreleaser
+                            sudo cp goreleaser /usr/local/bin
+                            '''
+                        }
+                    } catch (Exception e) {
+                        echo "Error during building GoReleaser: ${e.message}"
+                        currentBuild.result = 'FAILURE'
+                        error("Stopping pipeline due to failed GoReleaser build.")
+                    }
+                }
+            }
+        }
 
-				dir('go-containerregistry') {
-				    script {
-				    def Command1 = """sed -i '93,100d' .goreleaser.yml"""
-				    def Command2 = """sed -i '36s/^/  goamd64:\\n/' .goreleaser.yml"""
-				    def Command3 = """sed -i '37s/^/    - v3\\n/' .goreleaser.yml"""
-				    def Command4 = """sed -i '13s/^/  - GOEXPERIMENT=newinliner\\n/' .goreleaser.yml"""
-				    def Command5 = """sed -i '17s/^/  - -pgo=auto\\n/' .goreleaser.yml"""
-					sh 'go mod tidy'
-					sh Command1
-					sh Command2
-					sh Command3
-					sh Command4
-					sh Command5
-					sh 'goreleaser build --clean --snapshot --single-target --id crane'
-					sh 'upx -9 -q dist/crane_linux_amd64_v3/crane'
-					sh 'sudo cp dist/crane_linux_amd64_v3/crane /usr/local/bin'
-				}
-			}
+        stage('PHASE5: BUILD & DEPLOY REMAINING TOOLS') {
+            parallel {
+                stage('Build Go Container Registry') {
+                    steps {
+                        script {
+                            try {
+                                dir('go-containerregistry') {
+                                    sh '''
+                                    echo "Environment Variables:"
+                                    echo "LANG=$LANG"
+                                    echo "PATH=$PATH"
+                                    echo "GOOS=$GOOS"
+                                    echo "GOARCH=$GOARCH"
+                                    echo "GOAMD64=$GOAMD64"
+                                    echo "GOEXPERIMENT=$GOEXPERIMENT"
+                                    go mod tidy
+                                    sed -i '93,100d' .goreleaser.yml
+                                    sed -i '36s/^/  goamd64:\\n/' .goreleaser.yml
+                                    sed -i '37s/^/    - v3\\n/' .goreleaser.yml
+                                    sed -i '13s/^/  - GOEXPERIMENT=newinliner\\n/' .goreleaser.yml
+                                    sed -i '17s/^/  - -pgo=auto\\n/' .goreleaser.yml
+                                    goreleaser build --clean --snapshot --single-target --id crane
+                                    upx -9 -q dist/crane_linux_amd64_v3/crane
+                                    sudo cp dist/crane_linux_amd64_v3/crane /usr/local/bin
+                                    '''
+                                }
+                            } catch (Exception e) {
+                                echo "Error during building Go Container Registry: ${e.message}"
+                                currentBuild.result = 'FAILURE'
+                                error("Stopping pipeline due to failed Go Container Registry build.")
+                            }
+                        }
+                    }
+                }
 
-				dir ('trivy') {
-				    script {
-				def Command1 = """sed -i '14s/^/    goamd64:\\n/' goreleaser.yml"""
-				def Command2 = """sed -i '15s/^/      - v3\\n/' goreleaser.yml"""
-				def Command3 = """sed -i '12s/^/      - GOEXPERIMENT=newinliner\\n/' goreleaser.yml"""
-				def Command4 = """sed -i '6s/^/    flags:\\n/' goreleaser.yml"""
-				def Command5 = """sed -i '7s/^/      - -trimpath\\n/' goreleaser.yml"""
-				def Command6 = """sed -i '8s/^/      - -pgo=auto\\n/' goreleaser.yml"""
-				sh 'go mod tidy'
-				sh Command1
-				sh Command2
-				sh Command3
-				sh Command4
-				sh Command5
-				sh Command6
-				sh 'goreleaser build --clean --snapshot --single-target --id build-linux'
-				sh 'upx -9 -q dist/build-linux_linux_amd64_v3/trivy'
-				sh 'sudo cp dist/build-linux_linux_amd64_v3/trivy /usr/local/bin'
-			}
-		}
-	}
-}
+                stage('Build Trivy') {
+                    steps {
+                        script {
+                            try {
+                                dir('trivy') {
+                                    sh '''
+                                    echo "Environment Variables:"
+                                    echo "LANG=$LANG"
+                                    echo "PATH=$PATH"
+                                    echo "GOOS=$GOOS"
+                                    echo "GOARCH=$GOARCH"
+                                    echo "GOAMD64=$GOAMD64"
+                                    echo "GOEXPERIMENT=$GOEXPERIMENT"
+                                    go mod tidy
+                                    sed -i '14s/^/    goamd64:\\n/' goreleaser.yml
+                                    sed -i '15s/^/      - v3\\n/' goreleaser.yml
+                                    sed -i '6s/^/    flags:\\n/' goreleaser.yml
+                                    sed -i '7s/^/      - -trimpath\\n/' goreleaser.yml
+                                    sed -i '8s/^/      - -pgo=auto\\n/' goreleaser.yml
+                                    goreleaser build --clean --snapshot --single-target --id build-linux
+                                    upx -9 -q dist/build-linux_linux_amd64_v3/trivy
+                                    sudo cp dist/build-linux_linux_amd64_v3/trivy /usr/local/bin
+                                    '''
+                                }
+                            } catch (Exception e) {
+                                echo "Error during building Trivy: ${e.message}"
+                                currentBuild.result = 'FAILURE'
+                                error("Stopping pipeline due to failed Trivy build.")
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
-		stage('PHASE4: POST BUILD CLEAN WORKSPACE') {
-			steps {
-				cleanWs(deleteDirs: true, disableDeferredWipeout: true)
-			}
-		}
-	}
+        stage('PHASE6: POST BUILD CLEAN WORKSPACE') {
+            steps {
+                cleanWs(deleteDirs: true, disableDeferredWipeout: true)
+            }
+        }
+    }
 }
