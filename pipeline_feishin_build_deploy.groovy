0da4adf76e2f2ef634ed8815b6d4a4a1a56ddd80
Ivan Angelov <ivangotoy@gmail.com>
2024-06-12 16:54:49 +0300

    Feishin deployment job optimizations

--- pipeline_feishin_build_deploy.groovy
+++ pipeline_feishin_build_deploy.groovy
@@ -9,6 +9,9 @@ pipeline {
         timestamps()
         skipDefaultCheckout(true)
     }
+    environment {
+        DATE = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy-HH-mm-ss"))
+    }
     stages {
         stage('PHASE1: CLEAN AMD64') {
             steps {
@@ -17,31 +20,30 @@ pipeline {
         }
         stage('PHASE2: GIT CLONE FEISHIN') {
             steps {
-                sh "git clone --quiet --depth 1 -b development --single-branch https://github.com/jeffvli/feishin.git"
+                checkout([$class: 'GitSCM',
+                          branches: [[name: 'development']],
+                          userRemoteConfigs: [[url: 'https://github.com/jeffvli/feishin.git']]])
             }
         }
         stage('PHASE3: BUILD AND DEPLOY FEISHIN') {
             steps {
-                dir('feishin') {
-                    script {
-                        def DATE = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy-HH-mm-ss"))
+                script {
                         sh '''
-                        sed -i 's/ --mac//g' package.json
-                        npm install --legacy-peer-deps
-                        npm run package:pr
-	  	                ssh root@alder 'mkdir -p /var/www/html/files/feishin/feishin-$DATE'
-		                rsync release/build/Feishin-*-linux-x64.tar.xz release/build/Feishin-*-win-x64.exe root@alder:/var/www/html/files/feishin/feishin-$DATE/
-		                ssh root@alder chown -R http: /var/www/html/files/feishin/
-		                ssh root@alder 'bash /tmp/cleanup_builds.sh'
-		                '''
+                            sed -i 's/ --mac//g' package.json
+                            npm ci --legacy-peer-deps
+                            npm run package:pr
+                            ssh root@alder "mkdir -p /var/www/html/files/feishin/feishin-${DATE}"
+                            rsync release/build/Feishin-*-linux-x64.tar.xz release/build/Feishin-*-win-x64.exe root@alder:/var/www/html/files/feishin/feishin-${DATE}/
+                            ssh root@alder 'chown -R http: /var/www/html/files/feishin/ && bash /tmp/cleanup_builds.sh'
+                        '''
+                }
             }
         }
-    }
-}
-stage('PHASE4: LAST Clean WORKSPACE') {
+        stage('PHASE4: LAST Clean WORKSPACE') {
             steps {
                 cleanWs(deleteDirs: true, disableDeferredWipeout: true)
             }
         }
     }
 }
+
