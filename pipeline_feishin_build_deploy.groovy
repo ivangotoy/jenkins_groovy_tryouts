a87d8c206b7a0815f1df1377f50deb2fc36bd22e
Ivan Angelov <ivangotoy@gmail.com>
2024-07-11 12:13:16 +0300

    feishin improved

--- pipeline_feishin_build_deploy.groovy
+++ pipeline_feishin_build_deploy.groovy
@@ -1,16 +1,11 @@
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
-
 pipeline {
-    agent {
-        label 'ryzen9'
-    }
+    agent { label 'ryzen9' }
     options {
         timestamps()
         skipDefaultCheckout(true)
     }
     environment {
-        DATE = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy-HH-mm-ss"))
+        DATE = sh(returnStdout: true, script: "date +%d-%m-%Y-%H-%M-%S").trim()
     }
     stages {
         stage('PHASE1: CLEAN AMD64') {
@@ -20,36 +15,32 @@ pipeline {
         }
         stage('PHASE2: GIT CLONE FEISHIN') {
             steps {
-                script {
-                    dir('feishin') {
-                        checkout([$class: 'GitSCM',
-                                branches: [[name: 'development']],
-                                extensions: [[$class: 'CloneOption',
-                                            depth: 1,
-                                            noTags: true,
-                                            reference: '',
-                                            shallow: true]],
-                                userRemoteConfigs: [[url: 'https://github.com/jeffvli/feishin.git']]])
-                    }
+                dir('feishin') {
+                    checkout([$class: 'GitSCM',
+                        branches: [[name: 'development']],
+                        extensions: [[$class: 'CloneOption',
+                                      depth: 1,
+                                      noTags: true,
+                                      shallow: true]],
+                        userRemoteConfigs: [[url: 'https://github.com/jeffvli/feishin.git']]
+                    ])
                 }
             }
         }
         stage('PHASE3: BUILD AND DEPLOY FEISHIN') {
             steps {
-                script {
-                    dir('feishin') {
-                        sh '''
-                            sed -i 's/ --mac//g' package.json
-                            npm install node-abi@latest --legacy-peer-deps 
-                            npm ci --legacy-peer-deps
-                            npm run package:pr
-                            ssh root@alder "mkdir -p /var/www/html/files/feishin/feishin-${DATE}"
-                            rsync release/build/Feishin-*-linux-x64.tar.xz release/build/Feishin-*-win-x64.exe root@alder:/var/www/html/files/feishin/feishin-${DATE}/
-                            ssh root@alder 'chown -R http: /var/www/html/files/feishin/ && bash /tmp/cleanup_builds.sh'
-                        '''
+                dir('feishin') {
+                    sh '''
+                        sed -i 's/ --mac//g' package.json
+                        npm install node-abi@latest --legacy-peer-deps
+                        npm ci --legacy-peer-deps
+                        npm run package:pr
+                        ssh root@alder "mkdir -p /var/www/html/files/feishin/feishin-${DATE}"
+                        rsync release/build/Feishin-*-linux-x64.tar.xz release/build/Feishin-*-win-x64.exe root@alder:/var/www/html/files/feishin/feishin-${DATE}/
+                        ssh root@alder 'chown -R http: /var/www/html/files/feishin/ && bash /tmp/cleanup_builds.sh'
+                    '''
                 }
             }
-        }
         }
         stage('PHASE4: LAST Clean WORKSPACE') {
             steps {
