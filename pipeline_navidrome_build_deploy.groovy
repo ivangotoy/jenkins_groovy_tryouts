f844b5e1efc3c552384bd30f11aa905860ba610a
Ivan Angelov <ivangotoy@gmail.com>
2024-07-11 12:06:55 +0300

    further optimize navidrome deployment

--- pipeline_navidrome_build_deploy.groovy
+++ pipeline_navidrome_build_deploy.groovy
@@ -10,45 +10,46 @@ pipeline {
         PATH = "/usr/local/go/bin:${env.PATH}"
     }
     stages {
-        stage('PHASE1: CLEAN AMD64') {
+        stage('Clean Workspace') {
             steps {
                 cleanWs(deleteDirs: true, disableDeferredWipeout: true)
             }
         }
-        
-        stage('PHASE2: GIT CLONE NAVIDROME') {
+        stage('Checkout Navidrome Repo') {
             steps {
-                script {
-                    dir('navidrome') {
-                        checkout([$class: 'GitSCM',
-                                  branches: [[name: 'master']],
-                                  extensions: [[$class: 'CloneOption', depth: 1, shallow: true]],
-                                  userRemoteConfigs: [[url: 'https://github.com/navidrome/navidrome.git']]
-                        ])
-                    }
+                dir('navidrome') {
+                    checkout([$class: 'GitSCM',
+                              branches: [[name: 'master']],
+                              extensions: [[$class: 'CloneOption',
+                                           depth: 1,
+                                           noTags: true,
+                                           reference: '',
+                                           shallow: true]],
+                              userRemoteConfigs: [[url: 'https://github.com/navidrome/navidrome.git']]])
                 }
             }
         }
-        
-        stage('PHASE3: BUILD AND DEPLOY NAVIDROME') {
+        stage('Build and Deploy Navidrome') {
             steps {
                 dir('navidrome') {
                     script {
                         sh '''
-                        sed -i '14s/^/    goamd64:\\n      - v3\\n      - -trimpath\\n      - -pgo=auto\\n/' .goreleaser.yml &&
-                        sed -i '8s/^/      - GOEXPERIMENT=newinliner\\n/' .goreleaser.yml &&
-                        make setup &&
-                        make buildjs &&
-                        goreleaser build --clean --snapshot --single-target --id navidrome_linux_amd64 &&
-                        upx -9 -q dist/navidrome_linux_amd64_linux_amd64_v3/navidrome &&
-                        rsync -aPz4 dist/navidrome_linux_amd64_linux_amd64_v3/navidrome root@router:/mnt/sda1/Music/
+                          sed -i '14s/^/    goamd64:\\n/' .goreleaser.yml &&
+                          sed -i '15s/^/      - v3\\n/' .goreleaser.yml &&
+                          sed -i '14s/^/      - -trimpath\\n/' .goreleaser.yml &&
+                          sed -i '15s/^/      - -pgo=auto\\n/' .goreleaser.yml &&
+                          sed -i '8s/^/      - GOEXPERIMENT=newinliner\\n/' .goreleaser.yml &&
+                          make setup &&
+                          make buildjs &&
+                          goreleaser build --clean --snapshot --single-target --id navidrome_linux_amd64 &&
+                          upx -9 -q dist/navidrome_linux_amd64_linux_amd64_v3/navidrome &&
+                          rsync -aPz4 dist/navidrome_linux_amd64_linux_amd64_v3/navidrome root@router:/mnt/sda1/Music/
                         '''
                     }
                 }
             }
         }
-        
-        stage('PHASE4: CLEAN WORKSPACE') {
+        stage('Clean Workspace After Build') {
             steps {
                 cleanWs(deleteDirs: true, disableDeferredWipeout: true)
             }
