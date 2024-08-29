ac5dcb72314ecc29e27ddb89314a5265cdf0fc58
Ivan Angelov <ivangotoy@gmail.com>
2024-08-29 08:45:54 +0300

    fix navidrome build after latest code updates

--- pipeline_navidrome_build_deploy.groovy
+++ pipeline_navidrome_build_deploy.groovy
@@ -34,11 +34,11 @@ pipeline {
                 dir('navidrome') {
                     script {
                         sh '''
-                          sed -i '14s/^/    goamd64:\\n/' .goreleaser.yml &&
-                          sed -i '15s/^/      - v3\\n/' .goreleaser.yml &&
-                          sed -i '14s/^/      - -trimpath\\n/' .goreleaser.yml &&
-                          sed -i '15s/^/      - -pgo=auto\\n/' .goreleaser.yml &&
-                          sed -i '8s/^/      - GOEXPERIMENT=newinliner\\n/' .goreleaser.yml &&
+                          sed -i '15s/^/    goamd64:\\n/' .goreleaser.yml &&
+                          sed -i '16s/^/      - v3\\n/' .goreleaser.yml &&
+                          sed -i '15s/^/      - -trimpath\\n/' .goreleaser.yml &&
+                          sed -i '16s/^/      - -pgo=auto\\n/' .goreleaser.yml &&
+                          sed -i '9s/^/      - GOEXPERIMENT=newinliner\\n/' .goreleaser.yml &&
                           make setup &&
                           make buildjs &&
                           goreleaser build --clean --snapshot --single-target --id navidrome_linux_amd64 &&
